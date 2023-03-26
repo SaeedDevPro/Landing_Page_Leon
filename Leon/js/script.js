b19933de367ba6e6c4183c27dd7fd7e9b79e6aef
SaeedDevPro <128771038+SaeedDevPro@users.noreply.github.com>
2023-03-27 00:17:33 +0300

    Add files via upload

---
+++ Leon/js/script.js
@@ -0,0 +1,6 @@
+let menuMobile = document.querySelector(".header .links");
+let btnMobile = document.querySelector(".icon");
+
+btnMobile.onclick = function () {
+    menuMobile.classList.toggle("activeMenu");
+}
